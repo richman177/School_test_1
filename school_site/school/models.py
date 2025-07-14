0c0ba410a40edb3dcfd62fb90f70f1892aabf674
Elmirbek Toktoraliev <elmirbektoktoraliev06@gmail.com>
2025-07-14 10:05:57 +0600

    Update models.py
    g

--- school_site/school/models.py
+++ school_site/school/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models 
 from phonenumber_field.modelfields import PhoneNumberField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
